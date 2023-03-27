06745b5d8d9fc364b0f946f5ca60ab47291d29c8
Gleb Fokin <g.fokin@skyeng.ru>
2023-03-27 13:53:53 +0700

    Добавляет вызов апи логина

--- components/login-component.js
+++ components/login-component.js
@@ -1,3 +1,5 @@
+import { login } from "../api.js";
+
 export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
   const appHtml = `
     <h1>Список задач</h1>
@@ -18,8 +20,13 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
   appEl.innerHTML = appHtml;
 
   document.getElementById("login-button").addEventListener("click", () => {
-    setToken("Bearer asb4c4boc86gasb4c4boc86g37k3bk3cg3c03ck3k37w3cc3bo3b8");
-
-    fetchTodosAndRender();
+    login({
+      login: "admin",
+      password: "admin",
+    }).then((user) => {
+      console.log(user);
+      setToken(`Bearer ${user.user.token}`);
+      fetchTodosAndRender();
+    });
   });
 }
