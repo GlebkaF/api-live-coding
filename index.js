a16241f8141630b651e29a2be97544c874137a60
Gleb Fokin <g.fokin@skyeng.ru>
2023-03-27 13:31:53 +0700

    Создает модуль API

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 //  * отрефакторить приложение на модули
 // 2. Реализовать форму регистрации
 
+import { addTodo, deleteTodo, getTodos } from "./api.js";
+
 // TODO: Получать из хранилища данных
 let tasks = [];
 
@@ -17,25 +19,10 @@ token = null;
 const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
 
 const fetchTodosAndRender = () => {
-  return fetch(host, {
-    method: "GET",
-    headers: {
-      Authorization: token,
-    },
-  })
-    .then((response) => {
-      if (response.status === 401) {
-        // token = prompt("Введите верный пароль");
-        // fetchTodosAndRender();
-        throw new Error("Нет авторизации");
-      }
-
-      return response.json();
-    })
-    .then((responseData) => {
-      tasks = responseData.todos;
-      renderApp();
-    });
+  return getTodos({ token }).then((responseData) => {
+    tasks = responseData.todos;
+    renderApp();
+  });
 };
 
 const renderApp = () => {
@@ -118,20 +105,14 @@ const renderApp = () => {
       const id = deleteButton.dataset.id;
 
       // подписываемся на успешное завершение запроса с помощью then
-      fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
-        method: "DELETE",
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .then((responseData) => {
-          // получили данные и рендерим их в приложении
-          tasks = responseData.todos;
-          renderApp();
-        });
+      deleteTodo({
+        id,
+        token,
+      }).then((responseData) => {
+        // получили данные и рендерим их в приложении
+        tasks = responseData.todos;
+        renderApp();
+      });
     });
   }
 
@@ -143,19 +124,10 @@ const renderApp = () => {
     buttonElement.disabled = true;
     buttonElement.textContent = "Задача добавляеятся...";
 
-    // подписываемся на успешное завершение запроса с помощью then
-    fetch(host, {
-      method: "POST",
-      body: JSON.stringify({
-        text: textInputElement.value,
-      }),
-      headers: {
-        Authorization: token,
-      },
+    addTodo({
+      text: textInputElement.value,
+      token,
     })
-      .then((response) => {
-        return response.json();
-      })
       .then(() => {
         // TODO: кинуть исключение
         textInputElement.value = "";
